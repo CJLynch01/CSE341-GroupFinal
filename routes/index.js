a044b6b5feb94da2397a16e2fc3904b853adf423
CJLynch01 <cjlynch@byui.edu>
2024-11-16 15:45:39 -0700

    Save

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.use('/', require('./swagger'));
+// router.use('/recipes', require('./recipes'));
+// router.use('/categories', require('./categories'));
+
+module.exports = router;
